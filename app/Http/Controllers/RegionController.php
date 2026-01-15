72b4b8e4f5ab331c7a5c34e4a978e96ee55d85cc
jjcastilla94 <alvarocastilaa49@gmail.com>
2026-01-15 14:22:59 +0100

    hechos los controller

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Region;
+use Illuminate\Http\Request;
+
+class RegionController extends Controller
+{
+    public function index() {
+        return Region::all();
+    }
+
+    public function show($id) {
+        return Region::findOrFail($id);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        return Region::create($request->all());
+    }
+
+    public function update(Request $request, $id) {
+        $region = Region::findOrFail($id);
+        $region->update($request->all());
+        return $region;
+    }
+
+    public function destroy($id) {
+        Region::destroy($id);
+        return response()->json(['message' => 'Región eliminada']);
+    }
+}
